b6390349f2fd3d3156606e24a66b37e2fef02fe5
parkjeyoung <josunyjostar@gmail.com>
2022-07-08 15:26:57 +0900

    test : 기본 레이아웃 설정

--- client/src/page/resume.tsx
+++ client/src/page/resume.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 import { Container } from "../components/styled/container";
 
+const Temp = styled(Container)`
+  background-color: #797979;
+  width: 100%;
+
+  transition: background-color 1.3s ease;
+  display: flex;
+
+  .header {
+    background-color: #8c3737;
+    height: 200px;
+  }
+  .banner {
+    background-color: #26447f;
+    height: 100px;
+  }
+  .content {
+    background-color: #437422;
+    height: calc(100vh - 500px);
+  }
+  .footer {
+    background-color: #be8c0f;
+    height: 200px;
+  }
+`;
+
 function Resume() {
-  return <Container></Container>;
+  return (
+    <Temp>
+      <header className="header">header</header>
+      <section className="banner">banner</section>
+      <section className="content">content</section>
+      <footer className="footer">footer</footer>
+    </Temp>
+  );
 }
 
 export default Resume;
